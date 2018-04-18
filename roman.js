1cad62947234abb814c55a34f42557d6d7291896
Tyler Yates <tyler.yates@utexas.edu>
2018-04-17 23:34:07 -0500

    Initial commit

---
+++ roman.js
@@ -0,0 +1,19 @@
+/**
+ * Converts a Roman Numeral string into its integer value.
+ *
+ * The following characters are supported:
+ * I = 1
+ * V = 5
+ * X = 10
+ * L = 50
+ * C = 100
+ * D = 500
+ * M = 1000
+ *
+ * @param {String} romanNumeral the given Roman Numeral string
+ *
+ * @return {Number} the integer value of the string
+ */
+function fromRoman(romanNumeral) {
+    // TODO implement
+}
